b56e45add5e0058d089bba008e6c40dffbcab09a
rakiraki829613-a11y <rakiraki829613@gmail.com>
2026-07-26 18:55:48 +0530

    add maximum consecutive ones solution

---
+++ maximum_consecutive_ones.py
@@ -0,0 +1,16 @@
+def maximum_cosecutive(nums):
+    count=0
+    max_count=0
+    for i in range(len(nums)):
+        if nums[i]==1:
+            count+=1
+            max_count=max(max_count,count)
+        else:
+            count=0
+    return max_count
+nums=[1,2,3]
+print(maximum_cosecutive(nums))
+
+
+        
+    
